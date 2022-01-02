76cd6af2fce50fc070fca97c50070613d8499657
Guruprasanna Shrinivas Hegde <gurs.hegde@gmail.com>
2022-01-01 23:20:06 -0500

    catalog microservice with docker compose.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetSection("DatabaseSettings").GetValue<string>("ConnectionString"));
+            var db = client.GetDatabase(configuration.GetSection("DatabaseSettings").GetValue<string>("Database"));
+            Products = db.GetCollection<Product>(configuration.GetSection("DatabaseSettings").GetValue<string>("CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
